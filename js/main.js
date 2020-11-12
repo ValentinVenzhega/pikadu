28e61890f8db7d9f6a087231759601a7a4311fde
ValentinVenяhega <valek917@yandex.ru>
2020-11-12 21:31:13 +0300

    commit3

--- js/main.js
+++ js/main.js
@@ -106,14 +106,50 @@ const setUsers = {
 
     handler();
   },
+
   // перебираем массив и ищем пользователя с таким email
   getUser(email) {
     return listUsers.find(item => item.email === email)
   },
+
   // записываем user в user
   autorizedUser(user) {
     this.user = user;
   }
+};
+
+// создаем посты
+const setPosts = {
+  allPosts: [
+    {
+      title: 'Заголовок поста',
+      text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+      tags: ['свежее', 'новое', 'горячее', 'мое', 'случайность'],
+      author: 'maks@mail.com',
+      date: '11.11.2020, 20:54:00',
+      like: 15,
+      comments: 20,
+    },
+    {
+      title: 'Заголовок поста2',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi aut veniam saepe minus dicta asperiores nisi quos perferendis deserunt maiores explicabo illum delectus adipisci eligendi a ipsum, repudiandae esse totam, ad incidunt nobis quam dolore. Sint eum ea nulla veritatis, laboriosam ex odit reiciendis nam repellat voluptates, molestias, accusamus in maiores corporis ut! Alias molestiae laboriosam voluptates deserunt cum quibusdam.',
+      tags: ['свежее', 'новое', 'мое', 'случайность'],
+      author: 'kate@mail.com',
+      date: '10.11.2020, 20:54:00',
+      like: 45,
+      comments: 10,
+    },
+    {
+      title: 'Заголовок поста3',
+      text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+      tags: ['свежее', 'новое', 'мое', 'случайность'],
+      author: 'kate@mail.com',
+      date: '10.11.2020, 20:54:00',
+      like: 55,
+      comments: 10,
+    }
+  ]
+
 };
 
 // действия после авторизации
@@ -135,9 +171,57 @@ const toggleAuthDom = () => {
 
 // добавляем посты
 const showAllPosts = () => {
-  postsWrapper.innerHTML = ''
+  let postsHTML = '';
+ // деструктивное присвоение { title, text, date }
+  setPosts.allPosts.forEach(({ title, text, date, tags, like, comments }) => {
 
-}
+    postsHTML += `
+    <section class="post">
+      <div class="post-body">
+        <h2 class="post-title">${title}</h2>
+        <p class="post-text">${text}</p>
+        <div class="tags">
+          <a href="#" class="tag">${tags}</a>
+        </div>
+      </div>
+      <div class="post-footer">
+        <div class="post-buttons">
+          <button class="post-button likes">
+            <svg width="19" height="20" class="icon icon-like">
+              <use xlink:href="img/icons.svg#like"></use>
+            </svg>
+            <span class="likes-counter">${like}</span>
+          </button>
+          <button class="post-button comments">
+            <svg width="21" height="21" class="icon icon-comment">
+              <use xlink:href="img/icons.svg#comment"></use>
+            </svg>
+            <span class="comments-counter">${comments}</span>
+          </button>
+          <button class="post-button save">
+            <svg width="19" height="19" class="icon icon-save">
+              <use xlink:href="img/icons.svg#save"></use>
+            </svg>
+          </button>
+          <button class="post-button share">
+            <svg width="17" height="19" class="icon icon-share">
+              <use xlink:href="img/icons.svg#share"></use>
+            </svg>
+          </button>
+        </div>
+        <div class="post-author">
+          <div class="author-about">
+            <a href="#" class="author-username">arteislamov</a>
+            <span class="post-time">5 минут назад</span>
+          </div>
+          <a href="#" class="author-link"><img src="img/avatar.jpeg" alt="avatar" class="author-avatar"></a>
+        </div>
+      </div>
+    </section>
+    `;
+  })
+  postsWrapper.innerHTML = postsHTML;
+};
 
 const init = () => {
 
@@ -195,7 +279,7 @@ menuToggle.addEventListener('click', function (event) {
   showAllPosts();
   toggleAuthDom();
 }
-
+// инициализация после полной загрузки сайта
 document.addEventListener('DOMContentLoaded', () => {
   init();
 });
